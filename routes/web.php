865b7fdfb9fda66c2f2a7e1d269861bec0ed3132
Eman Shbair <eman1872000@gmail.com>
2026-09-26 21:40:41 +0300

    Redirect root route to login/dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,10 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\App;
 
 Route::get('/', function () {
-    return view('welcome');
+    if (auth()->check()) {
+        return redirect()->route('dashboard');
+    }
+    return redirect()->route('login');
 });
 
 Route::get('/login', function () {
